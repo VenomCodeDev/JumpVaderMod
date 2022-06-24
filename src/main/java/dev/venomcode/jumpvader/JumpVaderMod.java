21b4c3e81aa7c6aa18a9ac87e2b101a5ef2291f3
VenomCodeDev <103589496+VenomCodeDev@users.noreply.github.com>
2022-06-24 05:23:08 -0500

    Fixed desync issue and added crafting recipe and drop table entries.

--- src/main/java/dev/venomcode/jumpvader/JumpVaderMod.java
+++ src/main/java/dev/venomcode/jumpvader/JumpVaderMod.java
@@ -1,5 +1,6 @@
 package dev.venomcode.jumpvader;
 
+import dev.venomcode.jumpvader.registers.BlockRegister;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -13,6 +14,6 @@ public class JumpVaderMod implements DedicatedServerModInitializer {
 	@Override
 	public void onInitializeServer()
 	{
-
+		BlockRegister.init();
 	}
 }
