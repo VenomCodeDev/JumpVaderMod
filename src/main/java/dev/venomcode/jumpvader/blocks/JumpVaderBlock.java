27066afab60fe2ec9c846cfc690141f1e8794c54
VenomCodeDev <103589496+VenomCodeDev@users.noreply.github.com>
2022-06-24 05:23:08 -0500

    Fixed desync issue and added crafting recipe and drop table entries.

--- src/main/java/dev/venomcode/jumpvader/blocks/JumpVaderBlock.java
+++ src/main/java/dev/venomcode/jumpvader/blocks/JumpVaderBlock.java
@@ -20,7 +20,7 @@ public class JumpVaderBlock extends Block implements BlockActionListener, BlockS
 {
     public JumpVaderBlock(  )
     {
-        super( FabricBlockSettings.of( Material.STONE ).strength( 4.0f ).requiresTool() );
+        super( FabricBlockSettings.of( Material.STONE ).strength( 4.0F ) );
     }
 
     @Override
---
+++ src/main/java/dev/venomcode/jumpvader/registers/BlockRegister.java
@@ -0,0 +1,23 @@
+package dev.venomcode.jumpvader.registers;
+
+import dev.venomcode.jumpvader.blocks.JumpVaderBlock;
+import dev.venomcode.vanillify.api.VanillaUtils;
+import dev.venomcode.vanillify.api.item.ServerBlockItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Items;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class BlockRegister
+{
+    public static void init()
+    {
+        Registry.register( Registry.BLOCK, JUMP_VADER_BLOCK.getIdentifier(), JUMP_VADER_BLOCK);
+
+        Registry.register( Registry.ITEM, JUMP_VADER_BLOCK.getIdentifier() ,new ServerBlockItem( JUMP_VADER_BLOCK, Items.SEA_LANTERN, JUMP_VADER_BLOCK.getIdentifier(), new FabricItemSettings().maxCount( 64 ) , VanillaUtils.getText( JUMP_VADER_BLOCK.getDisplayName(), Formatting.LIGHT_PURPLE ) ) );
+    }
+
+    public static final JumpVaderBlock JUMP_VADER_BLOCK = new JumpVaderBlock();
+}
